0eb99823e698d46ffda20743a117ea0a76775c91
nothingTVatYT <34131388+nothingTVatYT@users.noreply.github.com>
2024-04-18 13:11:37 +0200

    add dummy player info

--- core/src/net/nothingtv/gdx/objects/PlayerInfo.java
+++ core/src/net/nothingtv/gdx/objects/PlayerInfo.java
@@ -7,4 +7,11 @@ public class PlayerInfo {
     public String displayName;
     public TextureRegion icon;
     public PlayerObject playerObject;
+    private float currentHealth = 95;
+    public float getMaxHealth() {
+        return 100;
+    }
+    public float getCurrentHealth() {
+        return currentHealth;
+    }
 }
--- core/src/net/nothingtv/gdx/ui/PlayerInfoView.java
+++ core/src/net/nothingtv/gdx/ui/PlayerInfoView.java
@@ -3,10 +3,7 @@ package net.nothingtv.gdx.ui;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.Align;
 import net.nothingtv.gdx.objects.PlayerInfo;
 
@@ -18,21 +15,24 @@ public class PlayerInfoView extends Table {
     private final Image compass;
     private final Label location;
     private final PlayerInfo playerInfo;
+    private final ProgressBar healthBar;
 
     public PlayerInfoView(PlayerInfo player, Skin skin) {
         this.playerInfo = player;
         playerName = new Label(player.displayName, skin);
+        healthBar = new ProgressBar(0, player.getMaxHealth(), 1f, false, skin);
         fps = new Label("0000", skin);
         location = new Label("0000/0000", skin);
         playerIcon = new Image(player.icon);
         compass = new Image(new Texture(Gdx.files.internal("textures/arrow.png")));
         compass.setSize(24, 24);
         compass.setOrigin(Align.center);
-        add(playerName).colspan(3).row();
-        add(playerIcon).colspan(3).maxSize(64).row();
+        add(playerName);
+        add(fps).row();
+        add(playerIcon).colspan(2).maxSize(64).row();
+        add(healthBar).colspan(2).row();
         add(compass).maxSize(24);
         add(location);
-        add(fps);
         pack();
     }
 
@@ -42,6 +42,7 @@ public class PlayerInfoView extends Table {
         Vector3 currentPos = playerInfo.playerObject.getPosition();
         location.setText(String.format("%4.0f/%4.0f", currentPos.x, currentPos.z));
         compass.setRotation(playerInfo.playerObject.getRotation().getAngleAround(0, 1, 0));
+        healthBar.setValue(playerInfo.getCurrentHealth());
         super.act(delta);
     }
 }
